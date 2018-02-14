a1aa95a1d8482200f1917fa6f6b10879d0c4c4d8
AverinDS <awerin960@yandex.ru>
2018-02-14 14:08:57 +0400

    added firebase. Need fix conflict of version gms

---
+++ app/src/main/java/com/example/dmitry/appforsimbcourse/presenter/Presenter.kt
@@ -0,0 +1,8 @@
+package com.example.dmitry.appforsimbcourse.presenter
+
+/**
+ * Created by dmitry on 14.02.18.
+ */
+class Presenter {
+    fun
+}
--- app/src/main/java/com/example/dmitry/appforsimbcourse/view/MainActivity.kt
+++ app/src/main/java/com/example/dmitry/appforsimbcourse/view/MainActivity.kt
@@ -1,12 +1,19 @@
-package com.example.dmitry.appforsimbcourse
+package com.example.dmitry.appforsimbcourse.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dmitry.appforsimbcourse.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SING_IN :Int = 16773
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        List<AuthUI.>
+
+
     }
 }
