2f711688779e2376ae2dfe1cc229020cb895c436
AverinDS <awerin960@yandex.ru>
2018-02-20 22:30:14 +0400

    Added activity for editing personal data

--- app/src/main/java/com/example/dmitry/appforsimbcourse/view/PersonalDataActivity.kt
+++ app/src/main/java/com/example/dmitry/appforsimbcourse/view/PersonalDataActivity.kt
@@ -2,17 +2,12 @@ package com.example.dmitry.appforsimbcourse.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.example.dmitry.appforsimbcourse.R
 
-class ProfileActivity : AppCompatActivity() {
-
-    private lateinit var name:TextView
+class PersonalDataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-        name = findViewById(R.id.actProfileEmail)
-        name.text = intent.getStringExtra("email")
+        setContentView(R.layout.activity_personal_data)
     }
 }
